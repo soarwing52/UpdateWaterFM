2d3baf9cc22d7c7f2965612029d2e75d543a0409
CheYiHung <hung.che.yi@gmail.com>
2022-03-02 16:45:08 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace UpdateWaterFM
+{
+    class Program
+    {
+        static string WaterFMDir = @"C:\Ximple\WaterFMEntry";
+        static void Main(string[] args)
+        {
+            checkEnvironment();
+
+            Console.WriteLine("Hello World!");
+
+            bool checkEnvironment()
+            {
+                if (!Directory.Exists(WaterFMDir))
+                    return false;
+
+                Directory.CreateDirectory(Path.Combine(WaterFMDir, "Updates"));
+
+                return true;
+            }
+        }
+    }
+}
